462b320516bfee68d9ad9f9a53020bce0ac8c2fd
Jennifer Garcia <jen.garcia91@gmail.com>
2019-08-21 14:09:32 -0700

    Switching computers, added templates and working on code. we cool

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'lalalaalala'
+toolbar = DebugToolbarExtension(app)
+
 
 connect_db(app)
-db.create_all()
+
+
+@app.route("/")
+def list_user():
+    """ List users and give option to create user """
+
+    users = User.query.all()
+    return render_template("users.html", users=users)
+

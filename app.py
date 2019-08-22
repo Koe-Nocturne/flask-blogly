a50b168bdc1af419ba163e2b815358e1d0d5c921
Jonathan Lin <jonathanlin@LAPTOP-LH80P623.localdomain>
2019-08-22 11:09:59 -0700

    switch computers, updated app and templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 #from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -34,18 +34,25 @@ def list_user():
 def create_new_user_form():
     """form page for creating new user"""
 
-    return render_template('users/new/createuser.html')
+    return render_template('createuser.html')
 
 
 @app.route("/users/new", methods=["POST"])
 def create_new_redirect():
     """redirect page after creating new user"""
-    firstname = request.forms.get("first-name")
-    lastname = request.forms.get("last-name")
-    imgurl = request.forms.get("img-url")
+    firstname = request.form.get("first-name")
+    lastname = request.form.get("last-name")
+    imgurl = request.form.get("img-url")
+    # print("this is one of our requests", firstname)
+    if firstname == "":
+        flash("Must include first name")
+        return redirect('/users/new')
+    if lastname == "":
+        flash("Must include last name")
+        return redirect('/users/new')
+    
 
     rand = User(first_name=firstname, last_name=lastname, image_url=imgurl)
-    print(rand)
     db.session.add(rand)
     db.session.commit()
 
@@ -66,17 +73,29 @@ def edit_user_form(id):
 @app.route("/users/<int:id>/edit", methods=["POST"])
 def edit_user_redirect(id):
     """redirect page after editing user"""
+    user = User.query.get_or_404(id)
+    firstname = request.form.get("first-name")
+    lastname = request.form.get("last-name")
+    imgurl = request.form.get("img-url")
+    print("last name", lastname)
+    
+    if firstname != "":
+        user.first_name = firstname
+    if lastname != "":
+        user.last_name = lastname
+    if imgurl != "":
+        user.image_url = imgurl
 
+    db.session.add(user)
+    db.session.commit()
     return redirect('/users')
 
 
 @app.route("/users/<int:id>/delete", methods=["POST"])
 def delete_user_redirect(id):
-    """redirect page after deleting user"""
-    print("here")
+    """redirect page after deleting user"""    
     user = User.query.get_or_404(id)
     db.session.delete(user)
     db.session.commit()
-    print("deleted")
 
     return redirect('/users')
